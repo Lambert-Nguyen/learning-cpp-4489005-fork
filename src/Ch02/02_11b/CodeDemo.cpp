f6d6f6b1c83abe1accef47926a96c5402a6061cd
Lambert Nguyen <151005054+Lambert-Nguyen@users.noreply.github.com>
2025-03-17 06:49:38 +0000

    Type casting examples

--- src/Ch02/02_11b/CodeDemo.cpp
+++ src/Ch02/02_11b/CodeDemo.cpp
@@ -6,7 +6,21 @@
 #include <cstdint>
 
 int main(){
-    
+    int fahrenheit = 100;
+    int celcius;
+
+    celcius = ((float)5/9.0) * (fahrenheit-32);
+
+    std::cout << celcius << std::endl;
+
+    float weight = 10.99;
+
+    std::cout << weight << std::endl;
+    std::cout << (int)weight << std::endl;
+    std::cout << (int)((weight - (int)weight) * 10000) << std::endl;
+
+
+
     std::cout << std::endl << std::endl;
     return (0);
 }
